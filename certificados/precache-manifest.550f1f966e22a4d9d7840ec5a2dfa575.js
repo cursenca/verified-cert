0c9efad34b4ce024a9b5b44dd4b74e9571d32392
David Adriazola Muriel <daddriel@MacBook-Air-de-David.local>
2020-08-14 21:25:14 -0400

    add

---
+++ certificados/precache-manifest.550f1f966e22a4d9d7840ec5a2dfa575.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32732e0c0ff881feffac",
+    "url": "/css/about.adc53e8a.css"
+  },
+  {
+    "revision": "b822a24749a4bc64f1d7",
+    "url": "/css/app.53356440.css"
+  },
+  {
+    "revision": "0b8635f00c8f99957b61b6dc85b76336",
+    "url": "/img/fondo.0b8635f0.jpeg"
+  },
+  {
+    "revision": "b6dddafa04ba0312576b7b6fcfdf20c6",
+    "url": "/img/google.b6dddafa.svg"
+  },
+  {
+    "revision": "46e52231c45061ae89882b79279494f3",
+    "url": "/img/logosvg.46e52231.svg"
+  },
+  {
+    "revision": "d772a001dbe9513eeb03eb0a45aca1ca",
+    "url": "/img/pppp.d772a001.png"
+  },
+  {
+    "revision": "75b4c6a5d605f2e475ec1e1bab121dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32732e0c0ff881feffac",
+    "url": "/js/about.cfd322e1.js"
+  },
+  {
+    "revision": "b822a24749a4bc64f1d7",
+    "url": "/js/app.af77e222.js"
+  },
+  {
+    "revision": "e3839029cfbf79d76949",
+    "url": "/js/chunk-00c71944.1a0495b3.js"
+  },
+  {
+    "revision": "df5c7859c54832a82360",
+    "url": "/js/chunk-552217b4.b79d3f82.js"
+  },
+  {
+    "revision": "8724bdde013d7a6e8958",
+    "url": "/js/chunk-f2e3372a.4df301b4.js"
+  },
+  {
+    "revision": "61f70cbf5ec6252b6cd0",
+    "url": "/js/chunk-vendors.59d1263d.js"
+  },
+  {
+    "revision": "ca57cfb05acd08f9aa6e891ff38cfb14",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/vy.ico"
+  }
+]);
