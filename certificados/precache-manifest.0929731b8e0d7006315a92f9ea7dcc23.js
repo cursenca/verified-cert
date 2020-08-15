d5bf232a143fb50528d4e6323117a537b3b57950
David Adriazola Muriel <daddriel@MacBook-Air-de-David.local>
2020-08-15 04:11:58 -0400

    add

---
+++ certificados/precache-manifest.0929731b8e0d7006315a92f9ea7dcc23.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92606cdcd0ae7fe6ff60",
+    "url": "/certificados/css/about.5f9a227e.css"
+  },
+  {
+    "revision": "27b33e58316cccc3c7c0",
+    "url": "/certificados/css/app.dd6bcd70.css"
+  },
+  {
+    "revision": "0b8635f00c8f99957b61b6dc85b76336",
+    "url": "/certificados/img/fondo.0b8635f0.jpeg"
+  },
+  {
+    "revision": "b6dddafa04ba0312576b7b6fcfdf20c6",
+    "url": "/certificados/img/google.b6dddafa.svg"
+  },
+  {
+    "revision": "46e52231c45061ae89882b79279494f3",
+    "url": "/certificados/img/logosvg.46e52231.svg"
+  },
+  {
+    "revision": "273a5dea904829696e044ffeddab8307",
+    "url": "/certificados/img/powerpoint.273a5dea.svg"
+  },
+  {
+    "revision": "d772a001dbe9513eeb03eb0a45aca1ca",
+    "url": "/certificados/img/pppp.d772a001.png"
+  },
+  {
+    "revision": "2fb7294cf708cf859f93f8f99110e273",
+    "url": "/certificados/index.html"
+  },
+  {
+    "revision": "92606cdcd0ae7fe6ff60",
+    "url": "/certificados/js/about.804895e5.js"
+  },
+  {
+    "revision": "27b33e58316cccc3c7c0",
+    "url": "/certificados/js/app.e26a8cc6.js"
+  },
+  {
+    "revision": "68ff3d49e95ef6065dc5",
+    "url": "/certificados/js/chunk-00c71944.9e744b7b.js"
+  },
+  {
+    "revision": "61fb16d9d3bd9ded57d9",
+    "url": "/certificados/js/chunk-55199734.289620ce.js"
+  },
+  {
+    "revision": "52d99ac9bd5b14c52b16",
+    "url": "/certificados/js/chunk-f2e3372a.e89901ba.js"
+  },
+  {
+    "revision": "61f70cbf5ec6252b6cd0",
+    "url": "/certificados/js/chunk-vendors.59d1263d.js"
+  },
+  {
+    "revision": "7a28204eb2f544db2b6c9119320c5921",
+    "url": "/certificados/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/certificados/robots.txt"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/certificados/vy.ico"
+  }
+]);
